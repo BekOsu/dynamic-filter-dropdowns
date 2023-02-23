78dd9c265ffeda59e690fdc8c9558c350bc80cfb
abu <abubakersuliman@outlook.com>
2023-02-23 21:32:52 +0400

    TodoList table

---
+++ vehicles-project/database/migrations/2023_02_23_172650_create_todo_items_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('todo_items', function (Blueprint $table) {
+            $table->id();
+            $table->string('todo', 2000);
+            $table->boolean('completed');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('todo_items');
+    }
+};
